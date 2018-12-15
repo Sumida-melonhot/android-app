e445f0274e74b9ec5e02c687c8b7b8801159236f
Junseo Bang <junseo1020@gmail.com>
2018-12-15 13:17:47 +0900

    Initial Commit!

---
+++ app/src/main/java/space/codejun/hedwigapp/MainActivity.java
@@ -0,0 +1,13 @@
+package space.codejun.hedwigapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
